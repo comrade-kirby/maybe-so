0e95317fb3cfeb8f866dc5d15e6bdbe64eb7d7ec
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-11-19 14:25:08 -0500

    add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<footer>
+  <div class='footer-left'>
+    <h2>(MAYBE SO)</h2>
+    <p>Copyright 2021. All rights reserved.<br>Made with ♥ by Maybe So</p>
+  </div>
+  <div class='footer-right'>
+    <p>501 Eastern Parkway<br>Louisville, KY 40217</p>
+    <p>howdy@maybeso.studio</p>
+  </div>
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: var(--black);
+    padding: 60px 86px;
+  }
+
+  h2 {
+    font-family: EditorialNew;
+    font-size: 2.5rem;
+    margin-bottom: 60px;
+    color: white;
+  }
+
+  p {
+    color: white;
+    font-size: 1rem;
+  }
+
+  .footer-right {
+    align-self: flex-end;
+  }
+
+  .footer-right p {
+    margin-top: 24px;
+    text-align: right;
+  }
+</style>
