96420918bcbfc5e35da7ac7e5b068be33aff9994
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-11-18 19:47:15 -0500

    add can we help section

---
+++ src/lib/CanWeHelp.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Button from '$lib/button/Button.svelte'
+</script>
+
+<section>
+  <h1>
+		Can we help you with something? <span>(MAYBE SO)</span>
+	</h1>
+  <p>Think we might be a good fit? Reach out and say hi! Make sure to let us know a bit about yourself, your project, and any timeline or budget you may have in mind. </p>
+	<Button text='LET’S MAKE SOMETHING TOGETHER' />
+</section>
+
+<style>
+  section {
+    padding: 200px 235px;
+  }
+
+  span {
+    font-family: EditorialNew;
+		font-weight: 200;
+  }
+
+  p {
+    margin: 35px 0 75px;
+  }
+</style>
